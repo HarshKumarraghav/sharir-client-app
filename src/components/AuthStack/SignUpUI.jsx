0195e2c47d136435d8ac1b2118f79553d2c48dc0
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-06-06 03:34:02 +0530

    sign up functionality done

--- src/components/AuthStack/SignUpUI.jsx
+++ src/components/AuthStack/SignUpUI.jsx
@@ -1,10 +1,76 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {View, Text, Image, TouchableOpacity, TextInput} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {useNavigation} from '@react-navigation/native';
-const SignUpUI = () => {
-  const navigation = useNavigation();
+const SignUpUI = ({signupInfo, setSignupInfo, SinupHandler}) => {
+  const [validationErrors, setValidationErrors] = useState({});
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  useEffect(() => {
+    setIsButtonDisabled(
+      !signupInfo.username ||
+        !signupInfo.phone ||
+        !signupInfo.email ||
+        !signupInfo.password,
+    );
+  }, [signupInfo]);
+  const validateInput = () => {
+    const errors = {};
+
+    // Validate username
+    if (!signupInfo.username) {
+      errors.username = 'Please enter a username';
+    }
+
+    // Validate phone number
+    if (!signupInfo.phone) {
+      errors.phone = 'Please enter a phone number';
+    } else if (
+      signupInfo.phone.length !== 10 ||
+      !/^\d+$/.test(signupInfo.phone)
+    ) {
+      errors.phone = 'Please enter a valid 10-digit phone number';
+    }
+
+    // Validate email
+    if (!signupInfo.email) {
+      errors.email = 'Please enter an email address';
+    } else if (!/\S+@\S+\.\S+/.test(signupInfo.email)) {
+      errors.email = 'Please enter a valid email address';
+    }
+
+    // Validate password
+    if (!signupInfo.password) {
+      errors.password = 'Please enter a password';
+    } else if (signupInfo.password.length < 8) {
+      errors.password = 'Password must be at least 8 characters long';
+    } else if (!/\d/.test(signupInfo.password)) {
+      errors.password = 'Password must contain at least one number';
+    } else if (!/[a-z]/.test(signupInfo.password)) {
+      errors.password = 'Password must contain at least one lowercase letter';
+    } else if (!/[A-Z]/.test(signupInfo.password)) {
+      errors.password = 'Password must contain at least one uppercase letter';
+    } else if (!/\W/.test(signupInfo.password)) {
+      errors.password = 'Password must contain at least one special character';
+    }
+    setValidationErrors(errors);
+    // Return true if there are no validation errors
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSignUp = () => {
+    if (validateInput()) {
+      const reqData = {
+        username: signupInfo.username,
+        phonenumber: `+91${signupInfo.phone}`,
+        email: signupInfo.email,
+        password: signupInfo.password,
+      };
+      SinupHandler(reqData);
+      console.log('Sign up successful.', reqData);
+    } else {
+      console.log('Sign up failed. Please fix the validation errors.');
+    }
+  };
   return (
     <View className="flex-1 bg-background">
       <SafeAreaView className="flex">
@@ -35,34 +101,60 @@ const SignUpUI = () => {
           <Text className="ml-4 text-gray-700">User Name</Text>
           <TextInput
             className="p-4 bg-gray-100 text-gray-700 rounded-xl"
-            value={''}
+            value={signupInfo.username}
+            onChangeText={text =>
+              setSignupInfo({...signupInfo, username: text})
+            }
             placeholder="Enter the User Name"
           />
           <Text className="ml-4 text-gray-700">Enter the Phone Number</Text>
           <TextInput
             className="p-4 bg-gray-100 text-gray-700 rounded-xl"
-            value={''}
+            value={signupInfo.phone}
             placeholder="Phone Number"
             keyboardType="numeric"
             maxLength={10}
+            onChangeText={text => setSignupInfo({...signupInfo, phone: text})}
           />
+          {validationErrors.phone && (
+            <Text style={{color: 'red', marginLeft: 4}}>
+              {validationErrors.phone}
+            </Text>
+          )}
           <Text className="ml-4 text-gray-700">Email Address</Text>
           <TextInput
             className="p-4 bg-gray-100 text-gray-700 rounded-xl"
-            value={''}
+            value={signupInfo.email}
+            keyboardType="email-address"
+            onChangeText={text => setSignupInfo({...signupInfo, email: text})}
             placeholder="Enter the Email"
           />
+          {validationErrors.email && (
+            <Text style={{color: 'red', marginLeft: 4}}>
+              {validationErrors.email}
+            </Text>
+          )}
           <Text className="ml-4 text-gray-700">Password</Text>
           <TextInput
             className="p-4 bg-gray-100 text-gray-700 rounded-xl mb-5"
             secureTextEntry={true}
-            value={''}
+            value={signupInfo.password}
+            onChangeText={text =>
+              setSignupInfo({...signupInfo, password: text})
+            }
             placeholder="Enter the Password"
           />
-
+          {validationErrors.password && (
+            <Text style={{color: 'red', marginLeft: 4}}>
+              {validationErrors.password}
+            </Text>
+          )}
           <TouchableOpacity
-            className="py-3 bg-primary  flex justify-center rounded-xl"
-            onPress={() => navigation.navigate('home')}>
+            className={`py-3 bg-primary  flex justify-center rounded-xl  ${
+              isButtonDisabled ? 'opacity-50' : ''
+            }`}
+            onPress={handleSignUp}
+            disabled={isButtonDisabled}>
             <Text className="text-white text-xl font-bold text-center">
               Sign Up
             </Text>
--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,10 +1,61 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import SignUpUI from '../components/AuthStack/SignUpUI';
+import {SHARIR_API} from '../../Service/Api/API';
+import {useNavigation, NavigationProp} from '@react-navigation/native';
+type SignupInfo = {
+  username: string;
+  email: string;
+  phone: string;
+  password: string;
+};
+type RootStackParamList = {
+  login: undefined;
+};
+
+type SignUpScreenProps = {
+  navigation: NavigationProp<RootStackParamList, 'login'>;
+};
 
 const SignUpScreen = () => {
-  return <SignUpUI />;
+  const navigation = useNavigation<SignUpScreenProps['navigation']>();
+  const [signupInfo, setSignupInfo] = useState<SignupInfo>({
+    username: '',
+    email: '',
+    phone: '',
+    password: '',
+  });
+  const ClearForm = () => {
+    setSignupInfo({
+      username: '',
+      email: '',
+      phone: '',
+      password: '',
+    });
+  };
+  const SinupHandler = async (data: SignupInfo) => {
+    const RequestQptions = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data),
+    };
+    const response = await fetch(SHARIR_API + 'auth/register', RequestQptions);
+    const resdata = await response.json();
+    if (!response.ok) {
+      console.log(resdata);
+    } else {
+      ClearForm();
+      navigation.navigate('login');
+    }
+  };
+  return (
+    <SignUpUI
+      signupInfo={signupInfo}
+      setSignupInfo={setSignupInfo}
+      SinupHandler={SinupHandler}
+    />
+  );
 };
 
 export default SignUpScreen;
